6b8ea78a9f616ed45c94d3d11e00e2e57772c5f8
sandeep <tiwarisandeep1909@gmail.com>
2026-05-15 15:14:10 +0530

    initial project setup for url shortner

---
+++ src/test/java/com/sandeep/project2/urlshortner/UrlshortnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sandeep.project2.urlshortner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlshortnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
