c3a1d45111e7048a09873753309d60381f202e81
sandeep <tiwarisandeep1909@gmail.com>
2026-05-18 09:35:08 +0530

    created global exception handler class for exception handling

---
+++ src/main/java/com/sandeep/project2/urlshortner/exception/GlobalExceptionHandler.java
@@ -0,0 +1,9 @@
+package com.sandeep.project2.urlshortner.exception;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+
+}
