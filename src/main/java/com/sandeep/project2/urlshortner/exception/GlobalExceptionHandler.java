b03ba4a3f0cb74a505e52af23f583d245ca15d60
sandeep <tiwarisandeep1909@gmail.com>
2026-05-18 09:42:46 +0530

    added method in global exception handler for handling method arguement not valid exception

--- src/main/java/com/sandeep/project2/urlshortner/exception/GlobalExceptionHandler.java
+++ src/main/java/com/sandeep/project2/urlshortner/exception/GlobalExceptionHandler.java
@@ -1,9 +1,30 @@
 package com.sandeep.project2.urlshortner.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+        @ExceptionHandler(MethodArgumentNotValidException.class)
+        public ResponseEntity<Map<String, String>> handleInvalidRequest(
+                MethodArgumentNotValidException ex) {
 
-}
+            Map<String, String> error = new HashMap<>();
+
+            String message = ex.getBindingResult()
+                    .getFieldErrors()
+                    .get(0)
+                    .getDefaultMessage();
+
+            error.put("error", message);
+
+            return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+        }
+    }
