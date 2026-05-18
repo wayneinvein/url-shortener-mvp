9a18406fd218ad2469703a9b4955f93850a6fcf6
sandeep <tiwarisandeep1909@gmail.com>
2026-05-18 08:26:15 +0530

    added validation in longUrl field of url class

--- src/main/java/com/sandeep/project2/urlshortner/model/Url.java
+++ src/main/java/com/sandeep/project2/urlshortner/model/Url.java
@@ -3,6 +3,8 @@ package com.sandeep.project2.urlshortner.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -18,6 +20,12 @@ public class Url {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    //it'll make sure the input is in correct form
+    @NotBlank
+    @Pattern(
+            regexp = "^(http://|https://).+",
+            message = "URL must start with http:// or https://"
+    )
     private String longUrl;
 
     private String shortCode;
