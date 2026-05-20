df8869fe1d95dd006dadff34a87708c66f9ff227
sandeep <tiwarisandeep1909@gmail.com>
2026-05-20 16:10:11 +0530

    added 2 exception handlers in global exception handler

--- src/main/java/com/sandeep/project2/urlshortner/exception/GlobalExceptionHandler.java
+++ src/main/java/com/sandeep/project2/urlshortner/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.sandeep.project2.urlshortner.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -27,4 +28,22 @@ public class GlobalExceptionHandler {
 
             return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
         }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Map<String, String>> handleGenericException(Exception ex) {
+
+        Map<String, String> error = new HashMap<>();
+        error.put("error", "Something went wrong");
+
+        return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<Map<String, String>> handleBadJson(HttpMessageNotReadableException ex) {
+
+        Map<String, String> error = new HashMap<>();
+        error.put("error", "Invalid request format");
+
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
     }
