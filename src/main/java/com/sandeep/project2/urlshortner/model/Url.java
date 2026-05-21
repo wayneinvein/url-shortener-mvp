3509272ee0d1cc1e3c4fc4e01074639157dd0c17
sandeep <tiwarisandeep1909@gmail.com>
2026-05-21 23:48:50 +0530

    ensured uniqueness for shortcode in entity

--- src/main/java/com/sandeep/project2/urlshortner/model/Url.java
+++ src/main/java/com/sandeep/project2/urlshortner/model/Url.java
@@ -1,26 +1,26 @@
 package com.sandeep.project2.urlshortner.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import jakarta.persistence.Id;
 
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "url")
 public class Url {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String longUrl;
 
+    @Column(name = "short_code", nullable = false, unique = true)
     private String shortCode;
 }
