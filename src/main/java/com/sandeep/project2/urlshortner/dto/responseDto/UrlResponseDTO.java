9cd9079a9a8f10abdc2c41d696b3546580c6d4f5
sandeep <tiwarisandeep1909@gmail.com>
2026-05-22 23:59:32 +0530

    created response dto class

---
+++ src/main/java/com/sandeep/project2/urlshortner/dto/responseDto/UrlResponseDTO.java
@@ -0,0 +1,13 @@
+package com.sandeep.project2.urlshortner.dto.responseDto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+@AllArgsConstructor
+public class UrlResponseDTO {
+
+    private String shortUrl;
+}
