f0c26c03525ef04350845ce18259ba6186cb765a
sandeep <tiwarisandeep1909@gmail.com>
2026-05-24 02:18:30 +0530

    implemented caching using @EnableCaching and @Cacheable annotation

--- src/main/java/com/sandeep/project2/urlshortner/UrlshortnerApplication.java
+++ src/main/java/com/sandeep/project2/urlshortner/UrlshortnerApplication.java
@@ -2,12 +2,14 @@ package com.sandeep.project2.urlshortner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class UrlshortnerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UrlshortnerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(UrlshortnerApplication.class, args);
+    }
 
 }
